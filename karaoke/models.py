824bedbff9765e0ad8b93b835da086284984192b
Kim mingyu <minq0302@naver.com>
2023-05-07 14:21:41 +0900

    FEAT : 추가 노래방 호실 모델
    노래방 호실 모델인 Room모델 추가 완료

--- karaoke/models.py
+++ karaoke/models.py
@@ -26,3 +26,25 @@ class Karaoke(models.Model):
 
     def __int__(self):
         return self.id
+
+
+class Room(models.Model):
+    room_id = models.AutoField(primary_key=True, null=False, blank=False)
+    karaoke = models.ForeignKey(Karaoke, related_name="room", on_delete=models.CASCADE,
+                                db_column="id", verbose_name="노래방 id값")
+    roomNum = models.CharField(
+        blank=True, null=True, verbose_name='몇 번 방인지', max_length=5)
+    capacity = models.IntegerField(blank=True, null=True, verbose_name="몇인실인지")
+    remainSong = models.IntegerField(
+        blank=True, null=True, verbose_name="남은 곡 수")
+    remainTime = models.IntegerField(
+        blank=True, null=True, verbose_name='잔여 시간(분)')
+    status = models.IntegerField(
+        blank=True, null=True, verbose_name='잔여 곡 3개 이하거나 잔여 시간 10분 이하이면 1')
+
+    class Meta:
+        verbose_name = "노래방 호실"
+        verbose_name_plural = "노래방 호실들"
+
+    def __int__(self):
+        return self.room_id
